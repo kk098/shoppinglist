d212e638ae6546ed54345eeccc791afb6e451ecf
Florian Porada <florian.porada@gmail.com>
2016-06-01 20:00:15 +0200

    [BUGFIX] added leftover files

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/partials/:name', function(req, res, next) {
+    var name = req.params.name;
+    res.render('partials/' + name);
+});
+
+module.exports = router;
