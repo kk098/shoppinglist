d9757b730265658a93f4355e06c4480014a3d366
Florian Porada <florian.porada@gmail.com>
2016-06-26 14:32:07 +0200

    [BUGFIX] result for crawled items

--- api/validation/validation.controller.js
+++ api/validation/validation.controller.js
@@ -9,16 +9,20 @@ var Item = require('../item/item.model');
 exports.validate = function(req, res) {
     console.log(req.body);
 
-    var dbItem;
-
-    var name = (typeof req.body.object === 'string' ? req.body.object : req.body.object.label);
-    Item.findOne({name: name.toLowerCase()}, function (err, item) {
-        if (err) return handleError(err);
-
-        dbItem = item;
-
-        console.log(dbItem);
-    });
+    // var dbItem;
+    //
+    // var name = (typeof req.body.object === 'string' ? req.body.object : req.body.object.label);
+    // var check = validation.validateSpelling(name);
+    //
+    // check.then(function (res) {
+    //     Item.findOne({name: res}, function (err, item) {
+    //         if (err) return handleError(err);
+    //
+    //         dbItem = item;
+    //
+    //         console.log(dbItem);
+    //     });
+    // });
 
 
     var builtArray = _buildArray(req.body);
@@ -41,20 +45,28 @@ function _buildArray(data) {
     var deferred = defer();
 
     if (data.object.label && typeof data.object.label === 'string') {
-        results.push(data.object.label);
+        // Spellcheck label
+        var label = validation.validateSpelling(data.object.label);
 
-        if (data.object.value.aliases) {
-            _.each(data.object.value.aliases, function (keyword) {
-                results.push(keyword);
-            });
-        }
+        label.then(function (res) {
+            //push label in array for craweler
+            results.push(res);
 
-        deferred.resolve(_startSearch(results));
+            if (data.object.value.aliases) {
+                //push aliases in array for craweler
+                _.each(data.object.value.aliases, function (keyword) {
+                    results.push(keyword);
+                });
+            }
 
+            deferred.resolve(_startSearch(results));
+        });
     } else {
-        var check = validation.validateSpelling(data.object);
+        //spellcheck object
+        var object = validation.validateSpelling(data.object);
 
-        check.then(function (res) {
+        object.then(function (res) {
+            //push object in array for craweler
             results.push(res);
 
             deferred.resolve(_startSearch(results));
@@ -74,12 +86,15 @@ function _startSearch(keywords) {
     _.each(keywords, function (keyword) {
         if (typeof keyword !== 'string') { console.log("keyword isn't a string!"); return; }
 
+        // start crawler with every keyword
         var searchResult = searchApi.search(keyword);
 
+        // iterate over every vendor
         _.each(searchResult, function (vendorResults) {
             var items = vendorResults.itemInformation;
-
+            
             if (items.length > 0) {
+                // push every craweld item in resul array
                 _.each(items, function (item) {
                     results.push(item);
                 })
